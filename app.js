f93aa0d19d2c93a6ee464bd59b9b217d5afa23b2
Kirill Troitsky <V-Troitsky@yandex.ru>
2020-06-21 20:36:02 +0300

    third commit: added routes

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
+const usersArr = require("./routes/users")
+const cardsArr = require("./routes/cards");
 const { PORT = 3000 } = process.env;
-
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/", usersArr);
+app.use("/", cardsArr);
 
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`)
-})
+  console.log("server started")
+});
---
+++ routes/users.js
@@ -0,0 +1,19 @@
+const usersRouter = require("express").Router();
+const fs = require("fs");
+const path = require("path");
+const usersArray = path.join(__dirname, "../data/users.json");
+
+const users = (callback) => {
+  fs.readFile(usersArray, (err, data) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    return callback(JSON.parse(data));
+  });
+}
+usersRouter.get("/users", (req, res) => {
+  users((data) => res.send(data));
+});
+
+module.exports = usersRouter;
